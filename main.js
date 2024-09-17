dd3c9ee1f9abe8c56bd3fc46f816b4909fea8c38
Nkateko Shikuhele <[shikuhelenkateko@gmail.com]>
2024-09-17 16:18:28 +0200

    new book button and form display created

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 
+const myLibrary = ["book1", "book2", "book3"];
+
+function Book() {
+ 
+}
+
+function addBookToLibrary() {
+    const container = document.querySelector('.container');
+    myLibrary.forEach(book => {
+        const para = document.createElement('p')
+        para.textContent = book;
+        container.appendChild(para);
+    })
+}
+
+addBookToLibrary()
