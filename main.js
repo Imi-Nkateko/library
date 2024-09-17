4b68d3fd9616e45132695ff0d42456808059e1a3
Nkateko Shikuhele <[shikuhelenkateko@gmail.com]>
2024-09-17 16:20:23 +0200

    library container display created

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 
 const myLibrary = ["book1", "book2", "book3"];
+const newBookBtn = document.querySelector('#newBook-btn');
+const formEl = document.querySelector('form')
+
+
+// New book button to display form
+newBookBtn.addEventListener('click', () => {
+    formEl.style.display = 'block'
+})
+
 
 function Book() {
  
@@ -8,9 +17,11 @@ function Book() {
 function addBookToLibrary() {
     const container = document.querySelector('.container');
     myLibrary.forEach(book => {
-        const para = document.createElement('p')
-        para.textContent = book;
-        container.appendChild(para);
+        container.innerHTML += `
+        <div class="card-container">
+            <h1>${book}</h1>
+        </div>
+        `
     })
 }
 
